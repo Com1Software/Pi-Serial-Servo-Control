877c8407e42e9d3da0760d1377cc1b602011b2ba
dave@com1software.com <dave@com1software.com>
2023-07-22 17:06:55 -0400

    update

--- main.go
+++ main.go
@@ -2,21 +2,99 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"os"
+	"log"
+	"strings"
+
+	"go.bug.st/serial"
 )
 
 func main() {
+	fmt.Println("Land Vehicle Test System")
 
-	const serverPort = 8080
-
-	requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
-	res, err := http.Get(requestURL)
+	// Retrieve the port list
+	ports, err := serial.GetPortsList()
 	if err != nil {
-		fmt.Printf("error making http request: %s\n", err)
-		os.Exit(1)
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
 	}
 
-	fmt.Printf("client: got response!\n")
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	// Print the list of detected ports
+	for _, port := range ports {
+		fmt.Printf("Found port: %v\n", port)
+	}
+
+	// Open the first serial port detected at 9600bps N81
+	mode := &serial.Mode{
+		BaudRate: 115200,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+	port, err := serial.Open(ports[0], mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+
+		line := ""
+		buff := make([]byte, 1)
+		on := true
+		for on != false {
+			line = ""
+			for {
+				n, err := port.Read(buff)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if n == 0 {
+					fmt.Println("\nEOF")
+					break
+				}
+				line = line + string(buff[:n])
+				if strings.Contains(string(buff[:n]), "\n") {
+					break
+				}
+
+			}
+
+			ch1, ch2, ch3, ch4 := getCHPosition(line)
+			fmt.Print("\033[u\033[K")
+			fmt.Println("Land Vehicle Test System")
+			fmt.Printf("CH1=%s CH2=%s CH3=%s CH4=%s\n", ch1, ch2, ch3, ch4)
+			fmt.Println("======================================")
+
+		}
+	}
+}
+
+func getCHPosition(sentence string) (string, string, string, string) {
+	data := strings.Split(sentence, ",")
+	ch1 := ""
+	ch2 := ""
+	ch3 := ""
+	ch4 := ""
+	if len(data) == 4 {
+		ch1data := strings.Split(data[0], "=")
+		ch2data := strings.Split(data[1], "=")
+		ch3data := strings.Split(data[2], "=")
+		ch4data := strings.Split(data[3], "=")
+
+		if string(ch1data[0]) == "CH1" {
+			ch1 = ch1data[1]
+		}
+		if string(ch2data[0]) == "CH2" {
+			ch2 = ch2data[1]
+		}
+		if string(ch3data[0]) == "CH3" {
+			ch3 = ch3data[1]
+		}
+		if string(ch4data[0]) == "CH4" {
+			ch4 = ch4data[1]
+		}
+	}
+	return ch1, ch2, ch3, ch4
+
 }
